cb9e8f72e8f2a0246a0c7a66c58e43c110a6d928
karnita <ririnkarnita3@gmail.com>
2018-05-22 23:30:21 +0700

    Database dari gue

--- db/db_learning.sql
+++ db/db_learning.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 11, 2018 at 06:56 
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.24
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2018 at 06:17 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,53 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_learning`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `id_about` int(10) NOT NULL,
+  `about_desc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id_about`, `about_desc`) VALUES
+(2, 'Yeaaaaaay');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audio`
+--
+
+CREATE TABLE `audio` (
+  `id_audio` int(10) NOT NULL,
+  `id_mapel` int(10) NOT NULL,
+  `judul_audio` varchar(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `audio` varchar(100) NOT NULL,
+  `gambar` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `id_exam` int(10) NOT NULL,
+  `id_mapel` int(10) NOT NULL,
+  `soal` text NOT NULL,
+  `jawaban` varchar(200) NOT NULL,
+  `nilai` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -39,7 +86,41 @@ CREATE TABLE `foto` (
 
 INSERT INTO `foto` (`id`, `judul`, `deskripsi`, `cover`) VALUES
 (7, 'sda', 'safda', 'Screenshot_from_2018-04-28_16-00-18.png'),
-(9, 'asdas', 'sdadasd', 'icon.png');
+(9, 'asdas', 'sdadasd', 'icon.png'),
+(10, 'coba', 'ya', '16808-illustration-of-a-red-autumn-leaf-pv.png'),
+(11, 'hjk', 'fghj', 'Mewarnai_Gambar_Kepiting_(3).png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kuis`
+--
+
+CREATE TABLE `kuis` (
+  `id_kuis` int(10) NOT NULL,
+  `id_mapel` int(10) NOT NULL,
+  `soal` text NOT NULL,
+  `jawaban` varchar(200) NOT NULL,
+  `skor` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapel`
+--
+
+CREATE TABLE `mapel` (
+  `id_mapel` int(10) NOT NULL,
+  `mapel` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mapel`
+--
+
+INSERT INTO `mapel` (`id_mapel`, `mapel`) VALUES
+(1, 'Chemist');
 
 -- --------------------------------------------------------
 
@@ -57,7 +138,7 @@ CREATE TABLE `online_users` (
 --
 
 INSERT INTO `online_users` (`session`, `time`) VALUES
-('6eqsrp19u096108n6vlfv6lju05ua2lg', 1526057748);
+('g52f9mo3i031trkvpl4defub66pcvd8m', 1527005003);
 
 -- --------------------------------------------------------
 
@@ -79,15 +160,59 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
 (1, 'admin', '7488e331b8b64e5794da3fa4eb10ad5d', 'admin@admin.com');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video`
+--
+
+CREATE TABLE `video` (
+  `id_video` int(10) NOT NULL,
+  `judul_video` varchar(50) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `video_url` varchar(100) NOT NULL,
+  `gambar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id_about`);
+
+--
+-- Indexes for table `audio`
+--
+ALTER TABLE `audio`
+  ADD PRIMARY KEY (`id_audio`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`id_exam`);
 
 --
 -- Indexes for table `foto`
 --
 ALTER TABLE `foto`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kuis`
+--
+ALTER TABLE `kuis`
+  ADD PRIMARY KEY (`id_kuis`);
+
+--
+-- Indexes for table `mapel`
+--
+ALTER TABLE `mapel`
+  ADD PRIMARY KEY (`id_mapel`);
 
 --
 -- Indexes for table `online_users`
@@ -102,19 +227,55 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id_video`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id_about` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `audio`
+--
+ALTER TABLE `audio`
+  MODIFY `id_audio` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `exam`
+--
+ALTER TABLE `exam`
+  MODIFY `id_exam` int(10) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `kuis`
+--
+ALTER TABLE `kuis`
+  MODIFY `id_kuis` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mapel`
+--
+ALTER TABLE `mapel`
+  MODIFY `id_mapel` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `video`
+--
+ALTER TABLE `video`
+  MODIFY `id_video` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
